8e940820a07e97db0d8cd30af96217173bb8fdb1
ZoeyF75 <zf.zoodle@gmail.com>
2021-05-25 16:30:27 -0700

    File name fix, and method that turns bird color back to white on upclick

---
+++ Terrible Tweeters/Assets/Scripts/Bird.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {   
+        //prevents gravity from affecting bird at the start
+        GetComponent<Rigidbody2D>().isKinematic = true;
+    }
+
+    void OnMouseDown() 
+    {
+        //turns bird red when its clicked on
+        GetComponent<SpriteRenderer>().color = Color.red;
+    }
+
+    void OnMouseUp() 
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
