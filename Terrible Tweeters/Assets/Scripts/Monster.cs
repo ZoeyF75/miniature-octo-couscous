a5510e6635400b46dd9fd8a14a56997348e0d81d
ZoeyF75 <zf.zoodle@gmail.com>
2021-05-26 21:45:00 -0700

    Coroutine for m monster death, active becomes false after one second:

--- Terrible Tweeters/Assets/Scripts/Monster.cs
+++ Terrible Tweeters/Assets/Scripts/Monster.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[SelectionBase]
 public class Monster : MonoBehaviour
 {
     [SerializeField] Sprite _deadSprite;
@@ -13,7 +14,7 @@ public class Monster : MonoBehaviour
     {
         if (ShouldDieFromCollision(collision))
         {
-            Die();
+            StartCoroutine(Die());
         }
     }
 
@@ -32,11 +33,12 @@ public class Monster : MonoBehaviour
         return false;
     }   
 
-    void Die()
+    IEnumerator Die()
     {   
         _hasDied = true;
         GetComponent<SpriteRenderer>().sprite = _deadSprite;
         _particleSystem.Play();
-        // gameObject.SetActive(false);
+        yield return new WaitForSeconds(1);
+        gameObject.SetActive(false);
     }
 }
