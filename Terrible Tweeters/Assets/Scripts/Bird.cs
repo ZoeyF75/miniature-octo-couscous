c0230b2a01d104129e0727bf305c129ff6b7f933
ZoeyF75 <zf.zoodle@gmail.com>
2021-05-26 20:18:04 -0700

    Mouse drag restraint for right side of bird

--- Terrible Tweeters/Assets/Scripts/Bird.cs
+++ Terrible Tweeters/Assets/Scripts/Bird.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Bird : MonoBehaviour
 {   
     [SerializeField] float _launchForce = 500;
+    [SerializeField] float _maxDragDistance = 3.5;
 
     Vector2 _startPosition;
     Rigidbody2D _rigidbody2D;
@@ -48,10 +49,19 @@ public class Bird : MonoBehaviour
     {   
         //3D postional vector
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
         Vector2 desiredPosition = mousePosition;
+
+        float distance = Vector2.Distance(desiredPosition, _startPosition);
+        if (distance > _maxDragDistance) 
+        {
+            Vector2 direction = desiredPosition - _startPosition;
+            direction.Normalize();
+            desiredPosition =  _startPosition + (direction * _maxDragDistance);
+        }
+
         if (desiredPosition.x > _startPosition.x)
             desiredPosition.x = _startPosition.x;
+
         _rigidbody2D.position = desiredPosition;
     }
 
